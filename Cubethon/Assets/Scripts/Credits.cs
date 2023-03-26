f37fc308ae7d076994afbc1a364b3c4c440f98fe
dahmanp <dahmanp@my.erau.edu>
2023-03-26 11:26:13 -0700

    add project

---
+++ Cubethon/Assets/Scripts/Credits.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Credits : MonoBehaviour
+{
+    public void Quit()
+    {
+        Application.OpenURL("https://www.youtube.com/watch?v=mx86-rTclzA");
+    }
+}
